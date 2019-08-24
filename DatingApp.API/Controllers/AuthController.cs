417e3f0683c2cf0b922beedc485d0b41337f30ad
Natalija Ostojic <natalija.ostojic64@gmail.com>
2019-08-24 19:09:07 +0200

    Created User Entity. Created Auth Controller

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository  repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userName = userName.ToLower();
+
+            if(await _repo.UserExists(userName))
+            {
+                return BadRequest("UserName already exists");
+            }
+
+            var userToCreate = new User
+            {
+                UserName = userName
+            };
+
+            var createdUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
+        }
+    }
+}
